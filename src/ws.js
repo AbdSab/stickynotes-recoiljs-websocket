3537ba8d097f3509b853459a26a7dcd1a1e43e40
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2020-09-06 22:26:32 +0100

    removed some unused files

--- src/ws.js
+++ src/ws.js
@@ -1,5 +1,4 @@
 
-console.log(process.env.REACT_APP_WS_HOST || "http://localhost:8080");
 const ws = new WebSocket(process.env.REACT_APP_WS_HOST);
 ws.onopen = () => {
     console.log("WS Connexion Sucess");
